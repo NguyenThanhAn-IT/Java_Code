f4ad03a694cb706a0a35207ad496913bd02d1434
Nguyen Huu Nhan <82103041+NguyenHuuNhan1912@users.noreply.github.com>
2021-04-17 22:42:42 +0700

    DiemMau(Color_Point)

---
+++ DiemMau.java
@@ -0,0 +1,36 @@
+package Student_Management;
+import Buoi02.Diem;
+import java.util.Scanner;
+public class DiemMau extends Diem {
+	private String color;
+
+	public DiemMau(){
+		super();
+		color = new String();
+	}
+
+	public DiemMau(int a, int b, String m){
+		super(a,b);
+		color = new String(m);
+	}
+
+	public DiemMau(DiemMau mau){
+		super((Diem)mau);
+		color = mau.color;
+	}
+
+	public void readPoint(){
+		super.readPoint();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhap mau cua diem: ");
+		color = sc.nextLine();
+	}
+
+	public String toString(){
+		return super.toString() + ("\nDiem co mau: " + color);
+	}
+
+	public void gan(String mau){
+		color = new String(mau);
+	}
+}
